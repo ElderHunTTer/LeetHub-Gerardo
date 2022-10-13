f4dbc458aacf137a770654ada1314b58de3ee15a
ElderHunTTer <54445701+ElderHunTTer@users.noreply.github.com>
2022-10-12 22:08:57 -0500

    Time: 124 ms (7.13%), Space: 13.9 MB (30.01%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,48 @@
+# class Solution:
+#     def romanToInt(self, s: str) -> str:
+#         roman_numbers = {
+#             'I': 1, 'V': 5,
+#             'X': 10, 'L': 100,
+#             'C': 500, 'D': 500,
+#             'M': 1000,
+#         }
+#         self.s = [val for val in s]
+        
+#         rti = sum([roman_numbers[value] for index, value in enumerate(s)])
+
+#         return rti
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_dic = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+            }
+
+  
+        skip_next_n = False
+        int_sum = 0
+        for idx, n in enumerate(s):
+            if skip_next_n == True:
+                skip_next_n = False
+                continue
+            try:
+                if roman_dic[n] < roman_dic[s[idx + 1]]:
+                    int_sum +=  roman_dic[s[idx + 1]] - roman_dic[n]
+                    skip_next_n = True
+                    continue
+                else:
+                    int_sum += roman_dic[n]
+            except:
+                int_sum += roman_dic[n]
+                
+        return int_sum
